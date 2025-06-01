9399cab68f73d2c9ac4116cd856434c473fadfad
Courtney Daniels <205481698+cdjdaniels@users.noreply.github.com>
2025-06-01 14:29:26 -0700

    build

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/courtneydaniels/repos/my-website/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/courtneydaniels/repos/my-website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/courtneydaniels/repos/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/courtneydaniels/repos/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/courtneydaniels/repos/my-website/src/css/custom.css"),
+];
